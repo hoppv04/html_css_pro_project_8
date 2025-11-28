d6b49858034f4736cb4c2282c84ccd2a8f88c772
hoppv <hoppv04@gmail.com>
2025-11-28 15:18:24 +0700

    add form filter product ui

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -157,7 +157,9 @@ function initJsToggle() {
     if (!target) {
       document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
     }
-    button.onclick = () => {
+    button.onclick = (e) => {
+      e.preventDefault();
+
       if (!$(target)) {
         document.body.innerText = `Không tìm thấy phần tử "${target}"`;
         return;
@@ -168,6 +170,15 @@ function initJsToggle() {
         $(target).classList.toggle("hide", !isHidden);
         $(target).classList.toggle("show", isHidden);
       });
+    };
+
+    document.onclick = function (e) {
+      if (!e.target.closest(target)) {
+        const isHidden = $(target).classList.contains("hide");
+        if (!isHidden) {
+          button.click();
+        }
+      }
     };
   }
 }
